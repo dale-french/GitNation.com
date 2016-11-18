c31847d9fc4ba7139c030a81f9588c8797278962
Robert Haritonov <r@rhr.me>
2016-11-18 13:08:04 +0100

    exclude node_modules from ftp upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
-var gulp = require('gulp');
-var minimist = require('minimist');
-var args = minimist(process.argv.slice(2));
-
 var ftp = require('vinyl-ftp');
 var gutil = require('gulp-util');
 var minimist = require('minimist');
+
 var args = minimist(process.argv.slice(2));
+
 gulp.task('deploy', function() {
   var remotePath = '/domains/frontendamsterdam.com/';
   var conn = ftp.create({
@@ -14,7 +12,11 @@ gulp.task('deploy', function() {
     password: args.password,
     log: gutil.log
   });
-  gulp.src(['./**/*.*', '!./node_modules'])
+  gulp.src([
+      './**/*.*',
+      '!./.*',
+      '!./node_modules/**/*.*'
+    ])
     .pipe(conn.newer(remotePath))
     .pipe(conn.dest(remotePath));
 });
